013875a0daaa23b74e730d08e6bd0661c729f818
snehpnp <snehpnp@gmail.com>
2025-02-20 15:46:59 +0530

    Add Vercel Analytics integration and update .gitignore; include new dependency in package.json

--- src/App.js
+++ src/App.js
@@ -1,18 +1,10 @@
 import React, { useEffect, useRef, useState } from "react";
-import img1 from "./Images/1.jpg";
-import img2 from "./Images/2.jpg";
-import img3 from "./Images/3.jpg";
-import img4 from "./Images/4.jpg";
-import img5 from "./Images/5.jpg";
-import img6 from "./Images/6.jpg";
-import img7 from "./Images/7.jpg";
-import img8 from "./Images/8.jpg";
-import img9 from "./Images/9.jpg";
-import img10 from "./Images/10.jpg";
-import img11 from "./Images/11.jpg";
+
 import Carousel from "./Galary/Carousel";
 import Movingpage from "./Galary/Movingpage";
 import Doomtaana from "./Songs/Doomtaana.mp3";
+import { Analytics } from '@vercel/analytics/react';
+
 function importAll(r) {
   return r.keys().map((fileName) => r(fileName));
 }
@@ -175,6 +167,7 @@ function App() {
           </div>
         </section>
       )}
+      <Analytics />
     </div>
   );
 }
